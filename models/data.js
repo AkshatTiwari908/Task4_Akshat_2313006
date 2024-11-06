9813d9f8814dd8a73400535a15993dac9d253c13
ghost <akshattiwari487@gmail.com>
2024-11-07 00:54:40 +0530

    data

--- models/data.js
+++ models/data.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://akshattiwari487:RMZBaJUhdYPa22Lg@cluster0.uzydo.mongodb.net/notesDB?retryWrites=true&w=majority', {
+}).then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Failed to connect to MongoDB", err));
+
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
